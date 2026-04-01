49fcf42befee6506504cb52dc0b7ef396c05a43c
Bruno Rico <ricobruno58@gmail.com>
2026-04-01 17:54:41 -0400

    updates and prettying up code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:globally/auth/auth.dart';
 import 'package:globally/auth/login_or_register.dart';
